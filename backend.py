35a271a2ba4cdeab8704301143ef203f1c066c23
Phil Leonowens <phdrummer80@gmail.com>
2015-06-25 15:44:34 -0600

    Pagination with followers works

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from termcolor import colored
 import soundcloud
+import pdb
 
 #client = soundcloud.Client(client_id='8e906fb7c324fc6640fd3fc08ef9d1ff',client_secret='acd3a93bdfcf1dd65ed33497f091800', redirect_uri= url_for('logged_in'))
 #redirect client.authorize_url()
@@ -13,20 +14,31 @@ def main():
     )
     
     print(colored(client.get('/me').username, 'red'),'is logged in')
-    print( "1) Print all Following")
-    command = input("Enter Command:")
-    if command == "1":
-        count = 50  # Loops through followers because rate limited to 50
-        newFollowers = client.get('/me/followings/')
-        followers = newFollowers
-        while len( newFollowers ) >= 50:
-            newFollowers = client.get('me/followings/', offset=count)
-            followers + newFollowers
-            count= count+50
+    
+    
+    print( "1) Acquire Followings")
+    print( "2) Print Followings ")
+    command = "f" 
+    while command != "q":
+        command = input("Enter Command:")
+        if command == "1":
+            totalFollowers = list()
+            # start paging through results, 200 at a time
+            followers = client.get('/me/followings', limit=200,
+                                linked_partitioning=1)
+            totalFollowers = totalFollowers + [follower for follower in (followers.collection)]
+            previousResult = followers
 
-        print('You are following', colored( client.get('/me').followings_count,'green'),'users')
-        for follower in followers:
-            print(follower.username)
+            # if there are more than 200 followers, keeps getting them
+            while hasattr(previousResult,'next_href'):
+                followers = client.get(previousResult.next_href, limit=200,linked_partitioning=1) 
+                totalFollowers = totalFollowers + list(followers.collection)
+                previousResult = followers
+
+        if command == "2":
+            print('You are following', colored( client.get('/me').followings_count,'green'),'users')
+            for follower in totalFollowers:
+                print(follower['username'])
 
 if __name__ == '__main__':
     main()
