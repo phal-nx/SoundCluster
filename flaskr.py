a2f5a0b41d52cf0689064bdfc6a91b8b5b14d4ad
Phil Leonowens <phdrummer80@gmail.com>
2015-11-30 00:08:32 -0700

    Fixed authentication and added sessions

--- flaskr.py
+++ flaskr.py
@@ -6,8 +6,10 @@ from collections import Counter
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from flask import escape
 from flask import request
 from flask import redirect
+from flask import session
 from pymongo import MongoClient
 import pprint
 import os
@@ -16,36 +18,52 @@ import getpass
 import pdb
 
 
-#scClient= soundcloud.Client(client_id='8e906fb7c324fc6640fd3fc08ef9d1ff',client_secret='acd3a93bdfcf1dd65ed33497f091800', redirect_uri= url_for('logged_in'))
-
-
 app = Flask(__name__)
 app.debug = True
+
 # Initializes Mongo
 mongoClient = MongoClient()
 db = mongoClient.soundcloud
 
-
 client = soundcloud.Client(client_id='8e906fb7c324fc6640fd3fc08ef9d1ff',
     client_secret='aacd3a93bdfcf1dd65ed33497f091800',
-    redirect_uri='http://localhost:5000/profile/')#url_for('profilePage'))
+    redirect_uri='http://localhost:5000/authenticate')
 
 @app.route('/')
 def mainPage(entries=None):
-    #client = soundcloud.Client(client_id='8e906fb7c324fc6640fd3fc08ef9d1ff',
-    #   client_secret='aacd3a93bdfcf1dd65ed33497f091800',
-    #   redirect_uri='http://localhost:5000/profile/')#url_for('profilePage'))
+    if 'access_token' in session:
+        return redirect(url_for('profilePage'))
     return redirect(client.authorize_url())
 
-@app.route('/profile/')
-def profilePage():
-    code = request.form['code']
-    access_Token = client.exchange_token(code)
+@app.route('/authenticate')
+def authenticate():
+    #client = soundcloud.Client(client_id='8e906fb7c324fc6640fd3fc08ef9d1ff',
+    #    client_secret='aacd3a93bdfcf1dd65ed33497f091800',
+    #    redirect_uri='http://localhost:5000/profile')#url_for('profilePage'))
+    code = request.args.get('code')
+    token = client.exchange_token(code)
+    session['access_token'] = token.access_token
+    #for x in (attribute for attribute in dir(client.get('/me')) if not attribute.startswith('__')):
+        #if type(x) == 'str':
+    #    session[x] = str(client.get('/me').x)
+    return redirect(url_for('profilePage'))
 
-    client = soundcloud.Client(access_token=access_Token)
-    #currentUser = client.get('/me/') 
-    return render_template('profile.html')
-    return render_template('profile.html', currentuser=currentUser.username )
+@app.route('/logout')
+def logout():
+    session.pop('access_token',None)
+    return redirect(url_for('mainPage'))
+
+@app.route('/profile')
+def profilePage():
+    if 'access_token' not in session:
+        return redirect(url_for('mainPage'))
+    accessTok = session['access_token']
+    newClient = soundcloud.Client(access_token=accessTok)
+    currentLikes = newClient.get('/me/favorites')
+    currentTracks = newClient.get('/me/tracks')
+    currentUser = newClient.get('/me') 
+    return render_template('profile.html', user=currentUser, likes = currentLikes, tracks = currentTracks )
 
 if __name__ == '__main__':
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?QQ'
     app.run()
